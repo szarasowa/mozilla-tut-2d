6a3174d5d4a07ce6701db6893d110bfed9ea3e8b
emil <loremipsum@szarasowa.ovh>
2019-06-23 23:21:38 +0200

    3rd step: Bounce off the walls

--- scripts.js
+++ scripts.js
@@ -2,13 +2,24 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var fillColor = '#0095DD';
 
 function draw() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+    if (y + dy < ballRadius || y + dy > canvas.height-ballRadius) {
+        dy = -dy
+        fillColor = "#"+((1<<24)*Math.random()|0).toString(16);
+    }
+    if (x + dx < ballRadius || x + dx > canvas.width-ballRadius) {
+        dx = -dx;
+        fillColor = "#"+((1<<24)*Math.random()|0).toString(16); //  https://stackoverflow.com/a/5365036/7606077
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     x += dx;
     y += dy;
@@ -16,8 +27,8 @@ function draw() {
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x,y,10,0,Math.PI*2);
-    ctx.fillStyle = '#0095DD';
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = fillColor;
     ctx.fill();
     ctx.closePath();
 }
